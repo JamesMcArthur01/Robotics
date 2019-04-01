63cf9580abbc90d78e804c4b30e7de6a81b1afba
James McArthur <jamesmcarthur89@gmail.com>
2019-04-01 10:03:57 +0100

    Update Colour-Chaser.py

--- Colour-Chaser.py
+++ Colour-Chaser.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env/python
 
-import rospy, cv2, cv_bridge, numpy, math, actionlib, time
+"""
+CMP3103M Autonomous Mobile Robotics
+Assessment Item 1
+
+@author: James McArthur
+ID: 06094508
+"""
+
+import rospy, cv2, cv_bridge, numpy, math, actionlib
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from numpy import nanmean, nanmin, nansum
+from numpy import nanmean, nanmin
 from kobuki_msgs.msg import BumperEvent
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionFeedback
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 
 class ColourChaser:
 
     def __init__(self):
 
-        #Start an image display thread
+       
         cv2.startWindowThread()
 
-        #OpenCV bridge object to convert ROS images into OpenCV images
         self.bridge = cv_bridge.CvBridge()
         
         #Determine speed of robot       
         self.linear_vel = 0.8
         
-        #Instance of Twist message
         self.twist = Twist()
         
         #===============================Subscribers=========================================
@@ -30,7 +36,7 @@ class ColourChaser:
         
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_cb)
         
-        self.bump_sub = rospy.Subscriber('mobile_base/events/bumper', BumperEvent, bump_cb) #NEEDS bump callback
+        self.bump_sub = rospy.Subscriber('mobile_base/events/bumper', BumperEvent, self.bump_cb) #NEEDS bump callback
         
         #===============================Publishers=========================================
         
@@ -38,8 +44,9 @@ class ColourChaser:
         
 
         #===============================Colour List=========================================   
-    
-        self.colour_index = 0
+
+        #Used to iterate through self.colours
+        self.colour_index = 0 
         
         self.colours = ['Red', 'Green', 'Blue', 'Yellow']
         
@@ -49,68 +56,78 @@ class ColourChaser:
         #===============================Waypoints========================================= 
         
         self.points = [(0.22,4.49),(1.50,-1.07),(-4.32,1.35)] 
-        
-        self.point_index = 0
-        
-        self.search_timer = time.time() + 20
+
+        #Used to iterate through self.points
+        self.point_index = 0 
+
+        #Flag determines if randomly searching or moving to new position
+        self.searching = True 
         
 
-    def raw_cb(self, data):
-        
-        #if time.time() < self.search_timer:
+    def raw_cb(self, data):        
+
             #Raw image window
             cv2.namedWindow("window", 1)
+            
             #Thresholded image window
             cv2.namedWindow("mask", 2)
             
-            try:
-            #Converted from ROS image (data) to OpenCv image (BGR)
+            try:           
                 cv_image = self.bridge.imgmsg_to_cv2(data,desired_encoding='bgr8')
             
             except cv_bridge.CvBridgeError, e:
                 print(e)
-            #Change colour model from BGR to HSV 
+            
             hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)      
     
-            #Choose a colour to threshold
+            #Select next colour to threshold
             mask = self.get_colour(hsv)
     
             width = cv_image.shape[1]
     
-            M = cv2.moments(mask) #centroid            
-    
-            if time.time() < self.search_timer:
+            M = cv2.moments(mask)               
+
+            #If no colour has been found yet, keep looking
+            if self.searching == True:
                 
-                if nanmin(self.laserArray > 0.75):
-                    
+                if nanmin(self.laser > 0.75):
+
+                    #Apply circle to objects center of mass and move towards it
                     if M['m00'] > 0:
                         cx = int(M['m10']/M['m00'])
                         cy = int(M['m01']/M['m00'])
                         cv2.circle(cv_image, (cx, cy), 20, (0,0,255), -1)
         
-                        error = cx - width/2 #difference between the center of the image and the centroid
-                        
+                        error = cx - width/2 
+
+                        #Adjust regarding the difference between center of image and the centroid
                         self.explore(self.linear_vel, -float(error) / 100)
         
-                        if(self.currentDist < 1.25) and (self.currentDist >= 0.75):
-                            
+                        if(self.center < 1.25) and (self.center >= 0.75):
+
+                            #Register colour as found, switch to next colour and mark search flag as false
                             self.found()
+
+                            #Check if all colours have been found
+                            self.colour_check()
         
                     else:
                         self.change_colour()
                         
-                        if nanmin(self.laserArray) < 1 and (math.isnan(self.currentDist) == False):
-                            
-                            self.avoid()
+                        if nanmin(self.laser) < 1 and (math.isnan(self.center) == False):
+
+                            #Avoid obstacles directly in front of turtlebot
+                            self.avoid()                       
                                            
                         else:
-                            self.explore(self.linear_vel, 0)
-                                             
+                            #Otherwise keep moving forward
+                            self.explore(self.linear_vel, 0)                                          
                 else:
                     self.explore(-1, 0)
             else:
-                self.go_to_waypoint(self.points[self.point_index])
-            
+                #Move to one of three different waypoints
+                self.go_to_waypoint(self.points[self.point_index])              
+                
             cv2.imshow("window", cv_image)
             cv2.waitKey(1)
             
@@ -118,18 +135,27 @@ class ColourChaser:
             cv2.waitKey(1)
         
                    
-                      
+    #Get ranges for the laser (left, right and center)                  
     def scan_cb(self, data):
-              
-        self.currentDist = data.ranges[len(data.ranges) / 2]
+                          
+        self.laser = data.ranges
         
-        self.laserArray = data.ranges
+        self.center = data.ranges[len(data.ranges) / 2]
         
-        self.right = self.laserArray[0]
+        self.right = self.laser[0]
         
-        self.left = self.laserArray[len(self.laserArray)-1]              
+        self.left = self.laser[len(self.laser)-1]
+    
+    #Reverse if bumper is pressed
+    def bump_cb(self,data):
         
-        
+        if(data.state == BumperEvent.PRESSED):
+            
+            print("oops! Think I hit something!")
+            
+            self.explore(-1, 1.5)                  
+    
+    #Move turtlebot based on linear and angular arguments
     def explore(self, v, a):
         
         self.twist.linear.x = v
@@ -137,7 +163,8 @@ class ColourChaser:
         self.twist.angular.z = a
         
         self.velocity_pub.publish(self.twist)
-        
+     
+    #If a colour is identified from dictionary, mark it as found and switch flag to goal pattern
     def found(self):
         
         if(self.colours[self.colour_index] == 'Red'):
@@ -145,6 +172,8 @@ class ColourChaser:
             self.haveFound['Red'] = True
             
             self.change_colour()
+            
+            self.searching = False
             
             print("I've found Red!")
 
@@ -154,6 +183,8 @@ class ColourChaser:
             
             self.change_colour()
             
+            self.searching = False
+            
             print("Found a green object!")
 
         elif(self.colours[self.colour_index] == 'Blue'):
@@ -161,6 +192,8 @@ class ColourChaser:
             self.haveFound['Blue'] = True
             
             self.change_colour()
+            
+            self.searching = False
             
             print("There's Blue!")
 
@@ -170,8 +203,12 @@ class ColourChaser:
             
             self.change_colour()
             
+            self.searching = False
+            
             print("Gotcha Yellow!")
-
+            
+    
+    #Changes colour on each callback until corresponding colour is found
     def change_colour(self):
         
         if self.colour_index < 3:
@@ -179,57 +216,59 @@ class ColourChaser:
             self.colour_index = self.colour_index + 1
             
         else:
-            self.colour_index = 0  
+            self.colour_index = 0             
                      
-    
+    #Avoid anything that falls inside condition statements
     def avoid(self):
 
-        if (math.isnan(nanmean(self.laserArray)) == False):
+        if (math.isnan(nanmean(self.laser)) == False):
+                
+            if nanmean(self.right) < 0.75:
+                self.explore(0,1.2)  
+       
+            elif nanmean(self.left) < 0.75:
+                self.explore(0,-1.2)
+                
+            elif math.isnan(self.left) == True and math.isnan(self.center) == True and math.isnan(self.right) == True:
             
-            if nanmean(self.currentDist) < 1.5 or nanmin(self.currentDist) < 1:
+                self.explore(-1, 1)
                 
-                self.explore(0, -1.2)
+            else:
+                self.explore(0, -1.2)                
                 
-                print("Front blocked - Turning Right")
-
-            elif nansum(self.left) < nansum(self.right):
-                
-                self.explore(0.3, 1)
-                
-                print("Right Blocked - Going Left")
-                
-            elif nansum(self.left) > nansum(self.right):
-                
-                self.explore(0.3, -1)
-                
-                print("Left Blocked - Going Right")
         else:
             self.explore(-1, 0.5)
-
+    
+    #set new colour bounds using colour model (depending on index value)
     def get_colour(self, colourModel):
 
-        if(self.colours[self.colour_index] == 'Red' and self.haveFound['Red'] == False):
+        if(self.colours[self.colour_index] == 'Red'\
+        and self.haveFound['Red'] == False):
             lower_bound = numpy.array([ 0, 200, 100])
             upper_bound = numpy.array([ 0, 255, 150])
                      
-        elif(self.colours[self.colour_index] == 'Green' and self.haveFound['Green'] == False):
+        elif(self.colours[self.colour_index] == 'Green'\
+        and self.haveFound['Green'] == False):
             lower_bound = numpy.array([ 40, 100,  80])
             upper_bound = numpy.array([ 80, 255, 250])
 
-        elif(self.colours[self.colour_index] == 'Blue' and self.haveFound['Blue'] == False):
+        elif(self.colours[self.colour_index] == 'Blue'\
+        and self.haveFound['Blue'] == False):
             lower_bound = numpy.array([ 100, 100,  100])
             upper_bound = numpy.array([ 130, 255, 255])
 
-        elif(self.colours[self.colour_index] == 'Yellow' and self.haveFound['Yellow'] == False):
+        elif(self.colours[self.colour_index] == 'Yellow'\
+        and self.haveFound['Yellow'] == False):
             lower_bound = numpy.array([ 30, 200,  100])
             upper_bound = numpy.array([ 50, 255, 150])
-        #white
+        #set to white
         else:
             lower_bound = numpy.array([ 255, 255, 255])
             upper_bound = numpy.array([ 255, 255, 255])
 
         return cv2.inRange(colourModel, lower_bound, upper_bound)
     
+    #Defines a new action to move to a new waypoint using SimpleActionClient
     def go_to_waypoint(self, wp):
         
         move_client = actionlib.SimpleActionClient('/move_base/', MoveBaseAction)
@@ -247,16 +286,42 @@ class ColourChaser:
         goal.target_pose.pose.orientation.y = 0.0
         goal.target_pose.pose.orientation.z = 0.0
         goal.target_pose.pose.orientation.z = -1.0
-        
-        
+              
         move_client.send_goal(goal)
         move_client.wait_for_result(rospy.Duration.from_sec(5.0))
         
-        if move_client.get_state() == actionlib.GoalStatus.SUCCEEDED:
-            print("WAHOO!")
+        #If turtlebot navigates successfully, update waypoint and start searching for colours       
+        if move_client.get_state() == actionlib.GoalStatus.SUCCEEDED:         
+            print("Made it to the goal!")
+            self.check_waypoint()
+            self.searching = True
+            
+    #updates waypoint (resets if greater than array length)                        
+    def check_waypoint(self):
+        
+        if self.point_index <= 1:
+            
             self.point_index = self.point_index + 1
-            self.search_timer = time.time() + 20
-           
+                  
+        else:
+            self.point_index = 0
+    
+    #Checks if all colours have been found
+    def colour_check(self):
+        
+        if self.haveFound['Red'] == True\
+        and self.haveFound['Green'] == True\
+        and self.haveFound['Blue'] == True\
+        and self.haveFound['Yellow'] == True:
+
+            #If all found, end the program
+            print("I've found all the colours!")
+            rospy.signal_shutdown("Program Completed")
+            
+            
+       
+
+#Instantiate and loop chaser object               
 def main():
        
     rospy.init_node('chaser')
